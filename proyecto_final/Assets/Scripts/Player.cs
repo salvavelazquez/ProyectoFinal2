16d62591b2f16eace49e292a3d0e5cd0eb9009e2
unknown <matiaseaviles@gmail.com>
2022-11-06 03:01:43 -0300

    Se agregó Limites de islas, suelos de islas, script para el player, limite del mundo

--- proyecto_final/Assets/Scripts/Player.cs
+++ proyecto_final/Assets/Scripts/Player.cs
@@ -4,34 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 15f;
-    public float horizontalInput;
-    public float forwardInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    public float speedV;
+    [SerializeField]
+    float speed = 3f;
+    float rotationSpeed = 5f;
+    float pitch;
+    
     void Update()
     {
-        UpdateMovement();
+
+      float horizontalInput = Input.GetAxis("Horizontal");
+      float verticalInput = Input.GetAxis("Vertical");
+
+      Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+      transform.position = transform.position + movementDirection * speed * Time.deltaTime;
+      // transform.Translate(movementDirectionspeedTime.deltaTime);
+
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection),rotationSpeed*Time.deltaTime);
+      
+     pitch -= speedV * Input.GetAxis("Mouse Y");
+
+     transform.eulerAngles = new Vector3(pitch, 0.0f , 0.0f);
     }
 
-    private void UpdateMovement()
-    {
-        //Configuracion de movimiento horizontal y vertical
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Jump");
-        transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * 50f, 0f) * Time.deltaTime);
-        transform.Rotate(new Vector3(Input.GetAxis("Vertical") * -50f, 0f, 0f) * Time.deltaTime);
-
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-        //Movimiento del tiburon hacia adelante
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-
-
-    }
 }
