2f3bccd3ac2bc0855be66d7c4429541b7cd938e6
salvavelazquez <111758179+salvavelazquez@users.noreply.github.com>
2022-11-13 20:06:39 -0300

    Actualizacion movimientos del player

--- proyecto_final/Assets/Scripts/TesorosAgarrados.cs
+++ proyecto_final/Assets/Scripts/TesorosAgarrados.cs
@@ -10,20 +10,34 @@ public class TesorosAgarrados : MonoBehaviour
    public Text textoTesoros;
 
    public GameObject Submarino;
+   public GameObject camSubmarino;
+   public GameObject camMain;
+
    private bool activo=true;
    private float speed = 2f;
-   private float limiteY = -5; 
+   private float limiteY = -5;
+   private bool estado = true;
 
-   private void Update() 
+
+    private void Update() 
    {
         textoTesoros.text = "Tesoros: " + tesoros.ToString() + "/5";
-        if (tesoros ==5 && activo ==true)
+        if (tesoros ==1 && activo ==true)
         {
           Submarino.transform.Translate(0, speed * Time.deltaTime,0);
+            if (estado)
+            {
+                Camera.main.gameObject.SetActive(false);
+                camSubmarino.SetActive(true);
+                estado = false;
+            }
         }
-        if (Submarino.transform.position.y > limiteY)
+        if (Submarino.transform.position.y > limiteY && activo == true)
         {
-           Submarino.transform.position = new Vector3 (1, limiteY, -280);
+            activo = false;
+            Submarino.transform.position = new Vector3 (1, limiteY, -280);
+            camSubmarino.SetActive(false);
+            camMain.SetActive(true);
         }
    }
     
